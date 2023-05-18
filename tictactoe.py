eb17c3ff64450143ac6985b74c34cdf3711e40f0
Ravin Pradhitya <90445721+ravinpradhitya@users.noreply.github.com>
2023-05-18 19:39:36 +0700

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -99,3 +99,104 @@ class AI:
         self.player = player
 
     # --- RANDOM ---
+    
+def rnd(self, board):
+        empty_sqrs = board.get_empty_sqrs()
+        idx = random.randrange(0, len(empty_sqrs))
+
+        return empty_sqrs[idx] 
+
+    # --- MINIMAX ---
+
+    def minimax(self, board, maximizing):
+        
+        # terminal case
+        case = board.final_state()
+
+        # player 1 wins
+        if case == 1:
+            return 1, None 
+
+        # player 2 wins
+        if case == 2:
+            return -1, None
+
+        # draw
+        elif board.isfull():
+            return 0, None
+
+        if maximizing:
+            max_eval = -100
+            best_move = None
+            empty_sqrs = board.get_empty_sqrs()
+
+            for (row, col) in empty_sqrs:
+                temp_board = copy.deepcopy(board)
+                temp_board.mark_sqr(row, col, 1)
+                eval = self.minimax(temp_board, False)[0]
+                if eval > max_eval:
+                    max_eval = eval
+                    best_move = (row, col)
+
+            return max_eval, best_move
+
+        elif not maximizing:
+            min_eval = 100
+            best_move = None
+            empty_sqrs = board.get_empty_sqrs()
+
+            for (row, col) in empty_sqrs:
+                temp_board = copy.deepcopy(board)
+                temp_board.mark_sqr(row, col, self.player)
+                eval = self.minimax(temp_board, True)[0]
+                if eval < min_eval:
+                    min_eval = eval
+                    best_move = (row, col)
+
+            return min_eval, best_move
+
+    # --- MAIN EVAL ---
+
+    def eval(self, main_board):
+        if self.level == 0:
+            # random choice
+            eval = 'random'
+            move = self.rnd(main_board)
+        else:
+            # minimax algo choice
+            eval, move = self.minimax(main_board, False)
+
+        print(f'AI telah memilih untuk menandai kotak di posisi {move} dengan hasil eval : {eval}')
+
+        return move 
+
+class Game:
+
+    def __init__(self):
+        self.board = Board()
+        self.ai = AI()
+        self.player = 1   #1-cross  #2-circles
+        self.gamemode = 'ai' 
+        self.running = True
+        self.show_lines()
+
+    # --- DRAW METHODS ---
+
+    def show_lines(self):
+        # bg
+        screen.fill( BG_COLOR )
+
+        # vertical
+        pygame.draw.line(screen, LINE_COLOR, (SQSIZE, 0), (SQSIZE, HEIGHT), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (WIDTH - SQSIZE, 0), (WIDTH - SQSIZE, HEIGHT), LINE_WIDTH)
+
+        # horizontal
+        pygame.draw.line(screen, LINE_COLOR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
+        pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT - SQSIZE), (WIDTH, HEIGHT - SQSIZE), LINE_WIDTH)
+
+    def draw_fig(self, row, col):
+        if self.player == 1:
+            # draw cross
+            # desc line
+            start_desc = (col * SQSIZE + OFFSET, row * SQSIZE + OFFSET)
+            end_desc = (col * SQSIZE + SQSIZE - OFFSET, row * SQSIZE + SQSIZE - OFFSET)
